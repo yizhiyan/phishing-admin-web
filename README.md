f81b04c89f9ad3c53727882d83205dc937d22639
yizhiyan <33596562+yizhiyan@users.noreply.github.com>
2023-03-22 17:53:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# phishing-admin-web
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-3xexsd)
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<style scoped></style>
